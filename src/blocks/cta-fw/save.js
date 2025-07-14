e2a47b0067305e15926b16172b8515c7bcd91a9d
ghgamble <grace.h.gamble@gmail.com>
2025-07-14 10:07:46 -0600

    added cta block

---
+++ src/blocks/cta-fw/save.js
@@ -0,0 +1,18 @@
+import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+
+export default function Save({ attributes }) {
+  const { backgroundColor = '#282b35' } = attributes;
+
+  const blockProps = useBlockProps.save({
+    className: 'cta-fw alignfull',
+    style: { backgroundColor },
+    role: 'region',
+    'aria-label': 'Full Width CTA',
+  });
+
+  return (
+    <div {...blockProps}>
+      <InnerBlocks.Content />
+    </div>
+  );
+}
