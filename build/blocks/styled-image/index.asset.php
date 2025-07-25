bddd9473f2407bf6153122a73f26e0fbe19d3568
ghgamble <grace.h.gamble@gmail.com>
2025-07-25 10:52:16 -0600

    adjust margins for parallax header

--- build/blocks/styled-image/index.asset.php
+++ build/blocks/styled-image/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '44673c7af7f695b9468c');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f8084168f11c84f547a6');
